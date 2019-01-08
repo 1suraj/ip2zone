46fc15ba1d54208a8f0783ea8990b708a1d62d3b
Suraj Nayak <1suraj.rims@gmail.com>
2019-01-08 12:10:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import pickle
 from flask import Flask,jsonify,request
 from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 
 clf= pickle.load(open('clf.pkl','rb'))
 
 
 @app.route("/api",methods=['POST'])
-CORS(app)
+
 
 def index():
     data = request.get_json(force=True)
